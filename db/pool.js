cd382f4512d982643acbf05e2a33ef478206252b
Ben <luben12337@gmail.com>
2025-01-07 22:31:48 +1100

    Updated database variable

--- db/pool.js
+++ db/pool.js
@@ -1,10 +1,8 @@
 const  { Pool } = require("pg");
+require("dotenv").config();
 
 module.exports = new Pool({
-    host: "localhost",
-    user: "odin",
-    database: "miju",
-    password: "grind",
-    port: 5432
+    connectionString: process.env.DATABASE_URL,
+    // connectionString: postgresql://<role_name>:<role_password>@localhost:5432/top_users
 });
 
