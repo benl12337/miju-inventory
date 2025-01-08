03ca939e85851477b8269cc6ad05cfa764a80696
Ben <luben12337@gmail.com>
2025-01-08 12:55:50 +1100

    Updated required fields

--- db/pool.js
+++ db/pool.js
@@ -3,6 +3,5 @@ require("dotenv").config();
 
 module.exports = new Pool({
     connectionString: process.env.DATABASE_URL,
-    // connectionString: postgresql://<role_name>:<role_password>@localhost:5432/top_users
 });
 
